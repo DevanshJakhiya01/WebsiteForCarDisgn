4e173d9c59a78188063fdd42de412fea216bc1e0
DevanshJakhiya01 <devanshjakhiya01@hotmail.com>
2025-01-13 10:37:13 +0530

    for signup.php

--- signup.php
+++ signup.php
@@ -1,47 +1,36 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "car_customization";
+$username = "root"; 
+$password = "";     
+$dbname = "car_customization_db"; 
+
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$new_username = $_POST['new_username'];
-$new_password = $_POST['new_password'];
-
-$sql = "INSERT INTO users (username, password) VALUES ('$new_username', '$new_password')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Sign up successful!";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-?>
-
-<?php
-$conn = new mysqli('localhost', 'root', '', 'car_customization_db');
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$username = $_POST['username'];
-$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+if ($_SERVER["REQUEST_METHOD"] == "POST") { 
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Signup successful. You can now <a href='login_signup.html'>login</a>.";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    
+    $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+    $stmt->bind_param("ss", $username, $hashed_password); 
+
+    if ($stmt->execute()) {
+        echo "Signup successful. You can now <a href='login_signup.html'>login</a>.";
+    } else {
+        echo "Error: " . $stmt->error; 
+    }
+
+    $stmt->close();
 }
 
 $conn->close();
 ?>
-
