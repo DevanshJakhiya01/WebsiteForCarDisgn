6ea171b2046d307a29087582ba2ed41f3f99baee
DevanshJakhiya01 <devanshjakhiya01@hotmail.com>
2025-02-11 12:31:15 +0530

    Photos in image

--- sedan.php
+++ sedan.php
@@ -103,10 +103,10 @@
                 "sport" => "Images/Firefly%20Audi%20A3%20Blue%20in%20color%20with%20Sport%20Wheel%2010329.jpg",
                 "alloy" => "Images/Firefly%20Audi%20A3%20Blue%20in%20color%20with%20Alloy%20Wheel%2010329.jpg",
                 "black" => "Images/Firefly%20Audi%20A3%20Blue%20in%20color%20with%20Black%20Rims%2052221.jpg",
-                "candyred" => "Images/Firefly%20Audi%20A3%20Candy%20Red%20color%2045678.jpg",
-                "perlblue" => "Images/Firefly%20Audi%20A3%20Perl%20Blue%20color%2045678.jpg",
-                "detonagreen" => "Images/Firefly%20Audi%20A3%20Detona%20Green%20color%2045678.jpg",
-                "blacksparidematte" => "Images/Firefly%20Audi%20A3%20Black%20Sparide%20Matte%20color%2045678.jpg"
+                "candyred" => "Images/Firefly%20Audi%20A3%20candy%20red%20color%20car%2094718.jpg",
+                "perlblue" => "Images/Firefly%20Audi%20A3%20purl%20blue%20color%20car%2094718.jpg",
+                "detonagreen" => "Images/Firefly%20Audi%20A3%20Detona%20Green%20color%20car%2094718.jpg",
+                "blacksparidematte" => "Images/Firefly%20Audi%20A3%20Black%20Sparide%20Matte%20color%20car%2094718.jpg"
             ]
         ],
         [
